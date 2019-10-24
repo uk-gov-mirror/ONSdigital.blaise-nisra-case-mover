3322c35a86ebeb30088b8d11110d3e2104f9cac1
Catalina <catalina.garcia.mail@gmail.com>
2019-10-24 14:54:54 +0100

    Adds function to check files in NISRA ONS folder

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.6
+
+RUN pip install pipenv==8.2.7
+
+WORKDIR /usr/src/app
+
+COPY Pipfile Pipfile.lock ./
+RUN pipenv install --deploy --system
+
+COPY . .
+
+CMD [ "python", "blaise_nisra_mover.py" ]
---
+++ blaise_nisra_mover.py
@@ -0,0 +1,25 @@
+import pysftp
+import os
+
+
+def establish_sftp_connection():
+    try:
+        cnopts = pysftp.CnOpts()
+        cnopts.hostkeys = None
+        sftp = pysftp.Connection(os.getenv('SFTP_HOST'), username=os.getenv('SFTP_USERNAME'), password=os.getenv('SFTP_PASSWORD'), port=int(os.getenv('SFTP_PORT')), cnopts=cnopts)
+    except Exception as err:
+        print('Connection error:', err)
+        raise
+
+    return sftp
+
+
+def look_for_survey_folder(sftp):
+
+    return sftp.listdir('ONS')
+
+
+sftp = establish_sftp_connection()
+print(look_for_survey_folder(sftp))
+
+# def get_all_blaise_files(sftp):
