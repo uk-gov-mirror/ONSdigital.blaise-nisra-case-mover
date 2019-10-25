0ac26048c030ee49c81adfdf057aaaa804e8d574
Catalina <catalina.garcia.mail@gmail.com>
2019-10-25 15:09:48 +0100

    Adds exit code

--- blaise_nisra_mover.py
+++ blaise_nisra_mover.py
@@ -6,7 +6,6 @@ def establish_sftp_connection():
     try:
         cnopts = pysftp.CnOpts()
         cnopts.hostkeys = None
-        # sftp = pysftp.Connection("172.30.120.58", username="foo", password="pass", port=2222, cnopts=cnopts)
         sftp = pysftp.Connection(os.getenv('SFTP_HOST'), username=os.getenv('SFTP_USERNAME'), password=os.getenv('SFTP_PASSWORD'), port=int(os.getenv('SFTP_PORT')), cnopts=cnopts)
         print(sftp.listdir('ONS'))
     except Exception as err:
@@ -18,3 +17,4 @@ def establish_sftp_connection():
 
 sftp = establish_sftp_connection()
 print('This is working')
+exit(0)
