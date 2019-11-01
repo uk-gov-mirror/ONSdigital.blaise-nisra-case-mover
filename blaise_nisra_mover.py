2495743ec853bab723bd948bb57cdebd3ba3816b
Catalina <catalina.garcia.mail@gmail.com>
2019-11-01 14:06:33 +0000

    Attempts to copy one file

--- blaise_nisra_mover.py
+++ blaise_nisra_mover.py
@@ -6,8 +6,13 @@ def establish_sftp_connection():
     try:
         cnopts = pysftp.CnOpts()
         cnopts.hostkeys = None
-        sftp = pysftp.Connection(os.getenv('SFTP_HOST'), username=os.getenv('SFTP_USERNAME'), password=os.getenv('SFTP_PASSWORD'), port=int(os.getenv('SFTP_PORT')), cnopts=cnopts)
-        print(sftp.listdir('ONS'))
+        with pysftp.Connection(os.getenv('SFTP_HOST'),
+                               username=os.getenv('SFTP_USERNAME'),
+                               password=os.getenv('SFTP_PASSWORD'),
+                               port=int(os.getenv('SFTP_PORT')),
+                               cnopts=cnopts) as sftp:
+            print(sftp.listdir('ONS/opn1911a'))
+            sftp.get('ONS/opn1911a/OPN1911a.bdbx', '/mnt/data/OPN1911a.bdbx')
     except Exception as err:
         print('Connection error:', err)
         raise
