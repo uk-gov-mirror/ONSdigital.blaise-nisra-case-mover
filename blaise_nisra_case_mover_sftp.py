373f1c4d4ac4dca95af488bd02f6cd96c847f0ac
Catalina <catalina.garcia.mail@gmail.com>
2019-11-21 16:25:51 +0000

    Tries md5_hash for blob

--- blaise_nisra_case_mover_sftp.py
+++ blaise_nisra_case_mover_sftp.py
@@ -15,18 +15,21 @@ def establish_sftp_connection():
                                port=int(os.getenv('SFTP_PORT')),
                                cnopts=cnopts) as sftp:
 
-            survey_path = 'ONS/OPN/opn1911a/'
+            survey_destination_path = 'OPN/opn1911a/'
+            survey_source_path = 'ONS/' + survey_destination_path
+
             bucket_name = 'nisra-transfer'
 
-            file_list = sftp.listdir(survey_path)
+            # file_list = sftp.listdir(survey_path)
+            file_list = ['opn1911a.manifest', 'opn1911a.badi']
 
             for file in file_list:
                 print('Copying %s from SFTP server...', file)
-                sftp.get(survey_path + file, file)
+                sftp.get(survey_source_path + file, file)
 
                 upload_blob(bucket_name=bucket_name,
                             source_file_name=file,
-                            destination_blob_name=survey_path + file)
+                            destination_blob_name=survey_destination_path + file)
 
     except Exception as err:
         print('Connection error:', err)
@@ -42,6 +45,7 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob = bucket.blob(destination_blob_name)
 
     if blob.exists():
+        print(blob.md5_hash())
         print('Replacing with new version of {}.'.format(
             destination_blob_name))
     else:
