ccae9bc94740fbfb1acf176b9ae95e3645796ff3
Catalina <catalina.garcia.mail@gmail.com>
2019-11-28 18:12:05 +0000

    Gets MD5 hashes of file in container and same file in bucket
    This program needs to be able to check if files have changed so that they are not copied unnecessarily. This commit prints the hash for the same file when it is in the container's default storage and then when it is moved to the bucket. Note that once in the bucket the file is a google cloud 'blob'

--- blaise_nisra_case_mover_sftp.py
+++ blaise_nisra_case_mover_sftp.py
@@ -1,41 +1,55 @@
 import os
 import pysftp
-import filecmp
+import pybase64
+import hashlib
 from google.cloud import storage
 
 
-def establish_sftp_connection():
+def main():
+    # Config details
+    survey_destination_path = 'OPN/opn1911a/'
+    survey_source_path = 'ONS/' + survey_destination_path
+    bucket_name = 'nisra-transfer'
+    file_list = ['opn1911a.manifest']
+
     try:
+        print('Attempting sftp connection...')
         cnopts = pysftp.CnOpts()
         cnopts.hostkeys = None
-
         with pysftp.Connection(os.getenv('SFTP_HOST'),
                                username=os.getenv('SFTP_USERNAME'),
                                password=os.getenv('SFTP_PASSWORD'),
                                port=int(os.getenv('SFTP_PORT')),
                                cnopts=cnopts) as sftp:
-
-            survey_destination_path = 'OPN/opn1911a/'
-            survey_source_path = 'ONS/' + survey_destination_path
-
-            bucket_name = 'nisra-transfer'
-
-            # file_list = sftp.listdir(survey_path)
-            file_list = ['opn1911a.manifest', 'opn1911a.badi']
+            print('Connection established.')
 
             for file in file_list:
-                print('Copying %s from SFTP server...', file)
+                print('Copying {} from SFTP server to container storage.'.format(file))
                 sftp.get(survey_source_path + file, file)
 
-                upload_blob(bucket_name=bucket_name,
-                            source_file_name=file,
-                            destination_blob_name=survey_destination_path + file)
+                with open(file) as file_to_check:
+                    # Hash for local storage
+                    data = file_to_check.read()
+                    md5 = hashlib.md5(data.encode('utf-8')).digest()
+                    print('MD5 of {} is {} '.format(file, md5))
 
+                if file_same_as_bucket_file():
+                    print('Ignoring file {} because it has not changed'.format(file))
+                else:
+                    print('Uploading {} to bucket {}'.format(file, bucket_name))
+                    upload_blob(bucket_name=bucket_name,
+                                source_file_name=file,
+                                destination_blob_name=survey_destination_path + file)
     except Exception as err:
         print('Connection error:', err)
         raise
 
     return
+
+
+def file_same_as_bucket_file():
+    # Function to compare hashes
+    return False
 
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
@@ -45,7 +59,8 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob = bucket.blob(destination_blob_name)
 
     if blob.exists():
-        print(blob.md5_hash)
+        blob2 = bucket.get_blob('OPN/opn1911a/opn1911a.manifest')
+        print('MD5 in bucket: ', pybase64.b64decode(blob2.md5_hash))
         print('Replacing with new version of {}.'.format(
             destination_blob_name))
     else:
@@ -59,6 +74,6 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     return
 
 
-establish_sftp_connection()
+if __name__ == "__main__":
+    main()
 
-exit(0)
