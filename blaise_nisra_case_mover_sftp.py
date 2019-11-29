6d3cbe0e63852e5b7bf63c84cf8b1a7e8da76a44
Catalina <catalina.garcia.mail@gmail.com>
2019-11-29 14:16:31 +0000

    main uses config.py variables

--- blaise_nisra_case_mover_sftp.py
+++ blaise_nisra_case_mover_sftp.py
@@ -2,19 +2,17 @@ import os
 import pysftp
 import pybase64
 import hashlib
+from config import survey_source_path, survey_destination_path
+from config import bucket_name, file_list
 from google.cloud import storage
 
 
 def main():
-    # Config details
-    survey_destination_path = 'OPN/opn1911a/'
-    survey_source_path = 'ONS/' + survey_destination_path
-    bucket_name = 'nisra-transfer'
-    file_list = ['opn1911a.manifest']
-    bucket = connect_to_bucket(bucket_name)
+
+    bucket = connect_to_bucket()
 
     try:
-        print('Attempting sftp connection...')
+        print('Attempting SFTP connection...')
         cnopts = pysftp.CnOpts()
         cnopts.hostkeys = None
         with pysftp.Connection(os.getenv('SFTP_HOST'),
@@ -25,7 +23,6 @@ def main():
             print('Connection established.')
 
             for file in file_list:
-
                 print('Copying {} from SFTP server to container storage.'.format(file))
                 sftp.get(survey_source_path + file, file)
 
@@ -38,9 +35,9 @@ def main():
                                 source_file_name=file,
                                 destination_blob_name=blob_destination_path)
                 elif file_same_as_bucket_file(file, file_blob):
-                    print('Ignoring file {} because it has not changed'.format(file))
+                    print('Ignoring file {} because its unchanged'.format(file))
                 else:
-                    print('Uploading {} to bucket {}'.format(file, bucket_name))
+                    print('Uploading file {} to bucket {}'.format(file, bucket_name))
                     upload_blob(bucket_name=bucket_name,
                                 source_file_name=file,
                                 destination_blob_name=blob_destination_path)
@@ -48,31 +45,33 @@ def main():
         print('Connection error:', err)
         raise
 
-    return
+    return 0
 
 
 def file_same_as_bucket_file(file, file_blob):
     """Compares a file and a blob using md5."""
 
-    file_blob_md5 = pybase64.b64decode(file_blob.md5_hash)
-    print('MD5 of {} in bucket is {} '.format(file_blob.name, file_blob_md5))
+    print('Comparing file {} to blob {}.'.format(file, file_blob.name))
 
-    with open(file) as file_to_check:
+    file_blob_md5 = pybase64.b64decode(file_blob.md5_hash)
+    print('MD5 of {} in bucket is {}.'.format(file_blob.name, file_blob_md5))
+
+    with open(file, 'rb') as file_to_check:
         data = file_to_check.read()
-        file_md5 = hashlib.md5(data.encode('utf-8')).digest()
-        print('MD5 of {} is {} '.format(file, file_md5))
+        file_md5 = hashlib.md5(data).digest()
+        print('MD5 of {} is {}.'.format(file, file_md5))
 
     if file_blob_md5 == file_md5:
         same_file = True
-        print('File {} and blob {} are the same'.format(file, file_blob.name))
+        print('File {} and blob {} are the same.'.format(file, file_blob.name))
     else:
         same_file = False
-        print('File {} and blob {} are different'.format(file, file_blob.name))
+        print('File {} and blob {} are different.'.format(file, file_blob.name))
 
     return same_file
 
 
-def connect_to_bucket(bucket_name):
+def connect_to_bucket():
 
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(bucket_name)
@@ -80,9 +79,9 @@ def connect_to_bucket(bucket_name):
     return bucket
 
 
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
+def upload_blob(source_file_name, destination_blob_name):
     """Uploads a file to the bucket. Replaces already existing blob"""
-    bucket = connect_to_bucket(bucket_name)
+    bucket = connect_to_bucket()
     blob = bucket.blob(destination_blob_name)
 
     blob.upload_from_filename(source_file_name)
