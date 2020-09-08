7b4837af0fc6f16b9fb4b832a5e18f3e6ab686dd
Paul Soule <22938608+pds2208@users.noreply.github.com>
2020-09-08 10:02:12 +0100

    Added stackdriver levels support

--- util/service_logging.py
+++ util/service_logging.py
@@ -1,11 +1,18 @@
 import logging.config
 from os import path
+import logging
+import google.cloud.logging
+from google.cloud.logging.handlers import CloudLoggingHandler, setup_logging
 
 
 def __init():
     __logger = logging.getLogger(__name__)
     log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.conf')
     logging.config.fileConfig(fname=log_file_path, disable_existing_loggers=False)
+
+    client = google.cloud.logging.Client()
+    handler = CloudLoggingHandler(client, name="BNCM")
+    setup_logging(handler)
     return __logger
 
 
