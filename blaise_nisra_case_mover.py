cf363cb46d853ae3fa3a04f87cf4d50a9e01e740
Matthew Palmer <matthewandrewpalmer@outlook.com>
2021-02-09 14:19:43 +0000

    ALL the fStrings

--- blaise_nisra_case_mover.py
+++ blaise_nisra_case_mover.py
@@ -21,15 +21,15 @@ googleStorage = GoogleStorage(bucket_name, log)
 @app.route('/')
 def main():
     log.info('Application started')
-    log.info('survey_source_path - ' + survey_source_path)
-    log.info('bucket_name - ' + bucket_name)
-    log.info('instrument_regex - ' + instrument_regex)
-    log.info('extension_list - ' + str(extension_list))
-    log.info('sftp_host - ' + sftp_host)
-    log.info('sftp_port - ' + sftp_port)
-    log.info('sftp_username - ' + sftp_username)
-    log.info('server_park - ' + server_park)
-    log.info('blaise_api_url - ' + blaise_api_url)
+    log.info(f'survey_source_path - {survey_source_path}')
+    log.info(f'bucket_name - {bucket_name}')
+    log.info(f'instrument_regex - {instrument_regex}')
+    log.info(f'extension_list - {str(extension_list)}')
+    log.info(f'sftp_host - {sftp_host}')
+    log.info(f'sftp_port - {sftp_port}')
+    log.info(f'sftp_username - {sftp_username}')
+    log.info(f'server_park - {server_park}')
+    log.info(f'blaise_api_url - {blaise_api_url}')
 
     googleStorage.initialise_bucket_connection()
     if googleStorage.bucket is None:
@@ -79,7 +79,7 @@ def get_instrument_folders(sftp, source_path):
     survey_folder_list = []
     for folder in sftp.listdir(source_path):
         if re.compile(instrument_regex).match(folder):
-            log.info('Instrument folder found - ' + folder)
+            log.info(f'Instrument folder found - {folder}')
             survey_folder_list.append(folder)
     return survey_folder_list
 
@@ -87,7 +87,7 @@ def get_instrument_folders(sftp, source_path):
 def process_instrument(sftp, source_path):
     instrument_name = source_path[-9:].strip('/')
     instrument_db_file = instrument_name + '.bdbx'
-    log.info('Processing instrument - ' + instrument_name)
+    log.info(f'Processing instrument - {instrument_name}')
     delete_local_instrument_files()
     instrument_files = get_instrument_files(sftp, source_path)
     if len(instrument_files) == 0:
@@ -107,7 +107,7 @@ def check_instrument_database_file_exists(instrument_files, instrument_name):
         return False
     for instrument_file in instrument_files:
         if instrument_file.lower() == instrument_name.lower() + '.bdbx':
-            log.info('Database file found - ' + instrument_file)
+            log.info(f'Database file found - {instrument_file}')
             return True
     return False
 
@@ -116,7 +116,7 @@ def delete_local_instrument_files():
     files = [file for file in os.listdir('.') if os.path.isfile(file)]
     for file in files:
         if any(fnmatch.fnmatch(file, pattern) for pattern in extension_list):
-            log.info('Deleting local instrument file - ' + file)
+            log.info(f'Deleting local instrument file - {file}')
             os.remove(file)
 
 
@@ -124,7 +124,7 @@ def get_instrument_files(sftp, source_path):
     instrument_file_list = []
     for instrument_file in sftp.listdir(source_path):
         if any(fnmatch.fnmatch(instrument_file, pattern) for pattern in extension_list):
-            log.info('Instrument file found - ' + instrument_file)
+            log.info(f'Instrument file found - {instrument_file}')
             instrument_file_list.append(instrument_file)
     return instrument_file_list
 
@@ -135,19 +135,22 @@ def check_if_matching_file_in_bucket(local_file, bucket_file_location):
         log.info(f'File {bucket_file} not found in bucket')
         return False
 
-    with open(local_file, 'rb') as local_file_to_check:
-        local_file_data = local_file_to_check.read()
-        local_file_md5 = hashlib.md5(local_file_data).digest()
-        log.info('Local file MD5 - ' + local_file + ' - ' + str(local_file_md5))
+    try:
+        with open(local_file, 'rb') as local_file_to_check:
+            local_file_data = local_file_to_check.read()
+            local_file_md5 = hashlib.md5(local_file_data).digest()
+            log.info(f'MD5 for Local file - {local_file} = {str(local_file_md5)}')
+    except (OSError, IOError) as e:
+        log.error(e, f'Failed to read local file - {local_file}')
 
     bucket_file_md5 = pybase64.b64decode(bucket_file.md5_hash).decode("utf-8")
-    log.info('Bucket file MD5 - ' + bucket_file.name + ' - ' + bucket_file_md5)
+    log.info(f'MD5 for Bucket file - {bucket_file.name} = {bucket_file_md5}')
 
     if local_file_md5 == bucket_file_md5:
-        log.info('Files match - ' + local_file + ' - ' + bucket_file.name)
+        log.info(f'Files {local_file} and {bucket_file.name} match')
         return True
     else:
-        log.info('Files do not match - ' + local_file + ' - ' + bucket_file.name)
+        log.info(f'Files do not match - {local_file} - {bucket_file.name}')
         return False
 
 
--- google_storage.py
+++ google_storage.py
@@ -13,18 +13,18 @@ class GoogleStorage:
 
     def initialise_bucket_connection(self):
         try:
-            self.log.info('Connecting to bucket - ' + self.bucket_name)
+            self.log.info(f'Connecting to bucket - {self.bucket_name}')
             storage_client = storage.Client()
             self.bucket = storage_client.get_bucket(self.bucket_name)
-            self.log.info('Connected to bucket - ' + self.bucket_name)
+            self.log.info(f'Connected to bucket - {self.bucket_name}')
         except Exception as ex:
             self.log.error('Connection to bucket failed - %s', ex)
 
     def upload_file(self, source, dest):
         blob_destination = self.bucket.blob(dest)
-        self.log.info('Uploading file - ' + source)
+        self.log.info(f'Uploading file - {source}')
         blob_destination.upload_from_filename(source)
-        self.log.info('Uploaded file - ' + source)
+        self.log.info(f'Uploaded file - {source}')
 
     def get_blob(self, blob_location):
         return self.bucket.get_blob(blob_location)
